46e5f257cfe49f9b89a40a34196daf82ee26f2e3
AaronGarman <aaron.a.garman14@gmail.com>
2024-07-24 21:29:39 -0400

    Set up movies API data retrieval
    Prints movie data to console for now.

--- MoviesApp/ContentView.swift
+++ MoviesApp/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var movies: [Movie] = [] // store movies retrieved from API
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +19,57 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear(perform: {
+            Task {
+                await fetchMovies()
+            }
+        })
+    }
+    
+    // function to retrieve movie data from API
+    
+    private func fetchMovies() async {
+        
+        // url for TMDB movies database "now playing" endpoint
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=b1446bbf3b4c705c6d35e7c67f59c413&language=en-US")!
+        do {
+
+            // data request
+            
+            // can loop through api w/ page numbers to get more movies - would use variable in api string for page number
+            
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let moviesResponse = try JSONDecoder().decode(MoviesResponse.self, from: data)
+
+            // get response data
+            
+            self.movies = moviesResponse.results
+
+            // print data for each movie - for testing
+            
+            printMovies()
+ 
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // function to print out all data for each movie
+    
+    private func printMovies() {
+        
+        print("total movies: \(movies.count)\n")
+        
+        for movie in movies {
+            print("Id: \(movie.id)")
+            print("Title: \(movie.title)")
+            print("Release Date: \(movie.releaseDate)")
+            print("Overview: \(movie.overview)")
+            print("Vote Average: \(movie.voteAverage)")
+            print("Poster Path: \(movie.posterPath)")
+            print("Backdrop Path: \(movie.backdropPath)\n")
+        }
     }
 }
 
---
+++ MoviesApp/MoviesResponse.swift
@@ -0,0 +1,34 @@
+//
+//  MoviesResponse.swift
+//  MoviesApp
+//
+//  Created by Aaron Garman on 7/24/24.
+//
+
+import Foundation
+
+struct MoviesResponse: Decodable {
+    let results: [Movie]    // movie API endpoint returns array of movie objects
+}
+
+struct Movie: Decodable {
+    let id: Int
+    let title: String
+    let releaseDate: String
+    let overview: String
+    let voteAverage: Double
+    let posterPath: String
+    let backdropPath: String
+    
+    // enum to convert snake case to camel case
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case releaseDate = "release_date"
+        case overview
+        case voteAverage = "vote_average"
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
